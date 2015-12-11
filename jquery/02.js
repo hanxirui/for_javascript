32c0e64050a0238de29d00f3ab705d6d77251ff2
hanxirui <han_xirui@163.com>
2015-12-11 18:02:00 +0800

    new demo

--- jquery/02.js
+++ jquery/02.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+	// 元素原则器
 	$('#selected-plays > li').addClass('horizontal');
 	$('#selected-plays > li:not(.horizontal)').addClass('sub-level');
+
+	// 属性选择器
+	$('a[href^="mailto:"]').addClass('mailto');
+    $('a[href$=".pdf"]').addClass('pdflink');
+    $('a[href^="http"][href*="henry"]').addClass('henrylink');
+
+// $('tr:even').addClass('alt');
+$('tr:nth-child(odd)').addClass('alt');
+$('td:contains(Henry)').addClass('highlight');
+
+
+
 });
